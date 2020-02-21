cfca498a63c7a65a3c94c9f1a1b9c65c784a745f
magicmanj27 <magicmanj27@gmail.com>
2020-02-21 06:43:13 -0700

    initial commit
     On branch master
     Changes to be committed:
    	new file:   Dockerfile
    	new file:   Dockerfile.dev
    	new file:   docker-compose.yaml
    	modified:   src/App.js
    	modified:   src/App.test.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there, this message is from Jeremy 🤯
         </p>
         <a
           className="App-link"
